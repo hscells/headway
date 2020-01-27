7a0bf5fa6e6ac54b08375cf24a0c413f3535d8f7
Harry Scells <harryscells@gmail.com>
2020-01-28 09:32:52 +1000

    Updates to client.

--- client.go
+++ client.go
@@ -7,29 +7,52 @@ import (
 )
 
 type Client struct {
-	name   string
 	host   string
+	secret string
 	client http.Client
 }
 
-func NewClient(host string, name string) *Client {
+func NewClient(host, secret string) *Client {
 	return &Client{
-		name:   name,
+		secret: secret,
 		host:   host,
 		client: http.Client{},
 	}
 }
 
-func (c *Client) Send(current, total float64, comment string) error {
+func (c *Client) Send(current, total float64, name, comment string) error {
 	req, err := http.NewRequest(http.MethodPut, c.host, nil)
 	if err != nil {
 		return err
 	}
 	q := req.URL.Query()
-	q.Add("name", c.name)
+	q.Add("name", name)
 	q.Add("current", fmt.Sprintf("%f", current))
 	q.Add("total", fmt.Sprintf("%f", total))
 	q.Add("comment", comment)
+	q.Add("secret", c.secret)
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return errors.New(fmt.Sprintf("invalid request: %d", resp.StatusCode))
+	}
+
+	return nil
+}
+
+func (c *Client) Message(message string) error {
+	req, err := http.NewRequest(http.MethodPut, c.host, nil)
+	if err != nil {
+		return err
+	}
+	q := req.URL.Query()
+	q.Add("message", message)
+	q.Add("secret", c.secret)
 	req.URL.RawQuery = q.Encode()
 
 	resp, err := c.client.Do(req)
